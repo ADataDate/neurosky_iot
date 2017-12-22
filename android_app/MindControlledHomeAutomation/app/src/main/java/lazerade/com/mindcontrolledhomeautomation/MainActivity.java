5635e0d9fbacc6937076b193c2cfd07335e33da2
ADataDate <mary.west@sparkfun.com>
2017-12-22 12:54:56 -0700

    Initial code commit for application

---
+++ android_app/MindControlledHomeAutomation/app/src/main/java/lazerade/com/mindcontrolledhomeautomation/MainActivity.java
@@ -0,0 +1,199 @@
+package lazerade.com.mindcontrolledhomeautomation;
+
+import android.bluetooth.BluetoothAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.neurosky.AlgoSdk.NskAlgoDataType;
+import com.neurosky.AlgoSdk.NskAlgoSdk;
+import com.neurosky.connection.ConnectionStates;
+import com.neurosky.connection.DataType.MindDataType;
+import com.neurosky.connection.TgStreamHandler;
+import com.neurosky.connection.TgStreamReader;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "Mindful";
+    private NskAlgoSdk nskAlgoSdk;
+    private TgStreamReader tgStreamReader;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nskAlgoSdk = new NskAlgoSdk();
+
+        try {
+            // (1) Make sure that the device supports Bluetooth and Bluetooth is on
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+                Toast.makeText(
+                        this,
+                        "Please enable your Bluetooth and re-run this program !",
+                        Toast.LENGTH_LONG).show();
+                //finish();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i(TAG, "error:" + e.getMessage());
+            return;
+        }
+
+
+        final TextView blinkDetected = (TextView) findViewById(R.id.blinkDetected);
+        blinkDetected.setText("Oh Bears!!!!");
+        final Button blinkDetect = (Button) findViewById(R.id.blinkDetect);
+        blinkDetect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blinkDetected.setText("Gimme that booty sex bears");
+            }
+        });
+
+        Button headSet = (Button) findViewById(R.id.headSet);
+        headSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Example of constructor public TgStreamReader(BluetoothAdapter ba, TgStreamHandler tgStreamHandler)
+                tgStreamReader = new TgStreamReader(mBluetoothAdapter,callback);
+
+                if(tgStreamReader != null && tgStreamReader.isBTConnected()){
+
+                    // Prepare for connecting
+                    tgStreamReader.stop();
+                    tgStreamReader.close();
+                }
+
+                // (4) Demo of  using connect() and start() to replace connectAndStart(),
+                // please call start() when the state is changed to STATE_CONNECTED
+                tgStreamReader.connect();
+            }
+        });
+
+    }
+
+    public void showToast(final String msg, final int timeStyle) {
+        MainActivity.this.runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), msg, timeStyle).show();
+            }
+
+        });
+    }
+
+    private TgStreamHandler callback = new TgStreamHandler() {
+
+        @Override
+        public void onStatesChanged(int connectionStates) {
+            // TODO Auto-generated method stub
+            Log.d(TAG, "connectionStates change to: " + connectionStates);
+            switch (connectionStates) {
+                case ConnectionStates.STATE_CONNECTING:
+                    // Do something when connecting
+                    break;
+                case ConnectionStates.STATE_CONNECTED:
+                    // Do something when connected
+                    tgStreamReader.start();
+                    showToast("Connected", Toast.LENGTH_SHORT);
+                    break;
+                case ConnectionStates.STATE_WORKING:
+                    // Do something when working
+
+                    //(9) demo of recording raw data , stop() will call stopRecordRawData,
+                    //or you can add a button to control it.
+                    //You can change the save path by calling setRecordStreamFilePath(String filePath) before startRecordRawData
+                    //tgStreamReader.startRecordRawData();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Button startButton = (Button) findViewById(R.id.startButton);
+                            startButton.setEnabled(true);
+                        }
+
+                    });
+
+                    break;
+                case ConnectionStates.STATE_GET_DATA_TIME_OUT:
+                    // Do something when getting data timeout
+
+                    //(9) demo of recording raw data, exception handling
+                    //tgStreamReader.stopRecordRawData();
+
+                    showToast("Get data time out!", Toast.LENGTH_SHORT);
+
+                    if (tgStreamReader != null && tgStreamReader.isBTConnected()) {
+                        tgStreamReader.stop();
+                        tgStreamReader.close();
+                    }
+
+                    break;
+                case ConnectionStates.STATE_STOPPED:
+                    // Do something when stopped
+                    // We have to call tgStreamReader.stop() and tgStreamReader.close() much more than
+                    // tgStreamReader.connectAndstart(), because we have to prepare for that.
+
+                    break;
+                case ConnectionStates.STATE_DISCONNECTED:
+                    // Do something when disconnected
+                    break;
+                case ConnectionStates.STATE_ERROR:
+                    // Do something when you get error message
+                    break;
+                case ConnectionStates.STATE_FAILED:
+                    // Do something when you get failed message
+                    // It always happens when open the BluetoothSocket error or timeout
+                    // Maybe the device is not working normal.
+                    // Maybe you have to try again
+                    break;
+            }
+        }
+
+        @Override
+        public void onRecordFail(int flag) {
+            // You can handle the record error message here
+            Log.e(TAG,"onRecordFail: " +flag);
+
+        }
+
+        @Override
+        public void onChecksumFail(byte[] payload, int length, int checksum) {
+            // You can handle the bad packets here.
+        }
+
+        @Override
+        public void onDataReceived(int datatype, int data, Object obj) {
+            // You can handle the received data here
+            // You can feed the raw data to algo sdk here if necessary.
+            //Log.i(TAG,"onDataReceived");
+            ProgressBar pBar = (ProgressBar) findViewById(R.id.progressBar);
+            switch (datatype) {
+                case MindDataType.CODE_ATTENTION:
+                    short attValue[] = {(short)data};
+                    nskAlgoSdk.NskAlgoDataStream(NskAlgoDataType.NSK_ALGO_DATA_TYPE_ATT.value, attValue, 1);
+                    pBar.setProgress(attValue[0]);
+                    break;
+                case MindDataType.CODE_MEDITATION:
+                    short medValue[] = {(short)data};
+                    nskAlgoSdk.NskAlgoDataStream(NskAlgoDataType.NSK_ALGO_DATA_TYPE_MED.value, medValue, 1);
+                    break;
+                case MindDataType.CODE_POOR_SIGNAL:
+                    short pqValue[] = {(short)data};
+                    nskAlgoSdk.NskAlgoDataStream(NskAlgoDataType.NSK_ALGO_DATA_TYPE_PQ.value, pqValue, 1);
+                    break;
+                case MindDataType.CODE_RAW:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    };
+}
