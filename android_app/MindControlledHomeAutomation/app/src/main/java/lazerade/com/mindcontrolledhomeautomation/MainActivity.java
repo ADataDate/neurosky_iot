bafbce5528d72ee60d8012ccc7741e8c46ea5596
ADataDate <mary.west@sparkfun.com>
2017-12-22 14:25:49 -0700

    Add Attention status and setting bar

--- android_app/MindControlledHomeAutomation/app/src/main/java/lazerade/com/mindcontrolledhomeautomation/MainActivity.java
+++ android_app/MindControlledHomeAutomation/app/src/main/java/lazerade/com/mindcontrolledhomeautomation/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private NskAlgoSdk nskAlgoSdk;
     private TgStreamReader tgStreamReader;
     private BluetoothAdapter mBluetoothAdapter;
+    private String connectionState;
+    private int seekState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nskAlgoSdk = new NskAlgoSdk();
-
+        Button headSet = (Button) findViewById(R.id.headSet);
         try {
             // (1) Make sure that the device supports Bluetooth and Bluetooth is on
             mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -40,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
                         "Please enable your Bluetooth and re-run this program !",
                         Toast.LENGTH_LONG).show();
                 //finish();
+            } else {
+                headSet.setVisibility(View.VISIBLE);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,18 +52,6 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-
-        final TextView blinkDetected = (TextView) findViewById(R.id.blinkDetected);
-        blinkDetected.setText("Oh Bears!!!!");
-        final Button blinkDetect = (Button) findViewById(R.id.blinkDetect);
-        blinkDetect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                blinkDetected.setText("Gimme that booty sex bears");
-            }
-        });
-
-        Button headSet = (Button) findViewById(R.id.headSet);
         headSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,88 +65,72 @@ public class MainActivity extends AppCompatActivity {
                     tgStreamReader.close();
                 }
 
-                // (4) Demo of  using connect() and start() to replace connectAndStart(),
-                // please call start() when the state is changed to STATE_CONNECTED
                 tgStreamReader.connect();
             }
         });
 
-    }
-
-    public void showToast(final String msg, final int timeStyle) {
-        MainActivity.this.runOnUiThread(new Runnable() {
-            public void run() {
-                Toast.makeText(getApplicationContext(), msg, timeStyle).show();
+        SeekBar sBar = findViewById(R.id.settingBar);
+        sBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d(TAG, "Setting seek state: " + progress);
+                seekState = progress;
             }
 
-        });
-    }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+         });
 
     private TgStreamHandler callback = new TgStreamHandler() {
 
         @Override
         public void onStatesChanged(int connectionStates) {
-            // TODO Auto-generated method stub
             Log.d(TAG, "connectionStates change to: " + connectionStates);
             switch (connectionStates) {
                 case ConnectionStates.STATE_CONNECTING:
-                    // Do something when connecting
+                    connectionState = "Connecting...";
                     break;
                 case ConnectionStates.STATE_CONNECTED:
                     // Do something when connected
                     tgStreamReader.start();
-                    showToast("Connected", Toast.LENGTH_SHORT);
+                    connectionState = "Connected";
                     break;
                 case ConnectionStates.STATE_WORKING:
-                    // Do something when working
-
-                    //(9) demo of recording raw data , stop() will call stopRecordRawData,
-                    //or you can add a button to control it.
-                    //You can change the save path by calling setRecordStreamFilePath(String filePath) before startRecordRawData
-                    //tgStreamReader.startRecordRawData();
-
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        public void run() {
-                            Button startButton = (Button) findViewById(R.id.startButton);
-                            startButton.setEnabled(true);
-                        }
-
-                    });
-
+                    connectionState = "Collecting Data";
                     break;
                 case ConnectionStates.STATE_GET_DATA_TIME_OUT:
-                    // Do something when getting data timeout
-
-                    //(9) demo of recording raw data, exception handling
-                    //tgStreamReader.stopRecordRawData();
-
-                    showToast("Get data time out!", Toast.LENGTH_SHORT);
-
+                    connectionState = "Time out waiting for data";
                     if (tgStreamReader != null && tgStreamReader.isBTConnected()) {
                         tgStreamReader.stop();
                         tgStreamReader.close();
                     }
-
                     break;
                 case ConnectionStates.STATE_STOPPED:
-                    // Do something when stopped
-                    // We have to call tgStreamReader.stop() and tgStreamReader.close() much more than
-                    // tgStreamReader.connectAndstart(), because we have to prepare for that.
-
+                    connectionState = "Stopped";
                     break;
                 case ConnectionStates.STATE_DISCONNECTED:
-                    // Do something when disconnected
+                    connectionState = "Disconnected";
                     break;
                 case ConnectionStates.STATE_ERROR:
-                    // Do something when you get error message
+                    connectionState = "Error";
                     break;
                 case ConnectionStates.STATE_FAILED:
-                    // Do something when you get failed message
-                    // It always happens when open the BluetoothSocket error or timeout
-                    // Maybe the device is not working normal.
-                    // Maybe you have to try again
+                    connectionState = "Failed";
                     break;
             }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    TextView connectionStatus = findViewById(R.id.connectionStatus);
+                    connectionStatus.setText(connectionState);
+                }
+            });
         }
 
         @Override
@@ -170,15 +147,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onDataReceived(int datatype, int data, Object obj) {
-            // You can handle the received data here
-            // You can feed the raw data to algo sdk here if necessary.
-            //Log.i(TAG,"onDataReceived");
             ProgressBar pBar = (ProgressBar) findViewById(R.id.progressBar);
             switch (datatype) {
                 case MindDataType.CODE_ATTENTION:
                     short attValue[] = {(short)data};
                     nskAlgoSdk.NskAlgoDataStream(NskAlgoDataType.NSK_ALGO_DATA_TYPE_ATT.value, attValue, 1);
+                    Log.d(TAG, "attValue: " + attValue[0]);
                     pBar.setProgress(attValue[0]);
+                    if (attValue[0] >= seekState) {
+                        Log.d(TAG, "Concentrated hard enough");
+                    }
                     break;
                 case MindDataType.CODE_MEDITATION:
                     short medValue[] = {(short)data};
@@ -194,6 +172,5 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         }
-
     };
 }
