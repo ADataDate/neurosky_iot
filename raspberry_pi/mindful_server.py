77fe121f786955bd4dad2bd989efcbca2a0c4c60
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2017-12-22 17:16:54 -0700

    Add RPi GPIO control

--- raspberry_pi/mindful_server.py
+++ raspberry_pi/mindful_server.py
@@ -3,14 +3,20 @@ import logging
 import traceback
 import sys
 
+import RPi.GPIO as GPIO
+
 PORT = 12345
+LED_PIN = 23
 
 if __name__ == "__main__":
-
     # Set logging format
     FORMAT = '[%(asctime)s %(filename)15s line: %(lineno)5s] %(message)s'
     logging.basicConfig(stream=sys.stdout, format=FORMAT, level=logging.DEBUG)
     log = logging.getLogger("mindful_server")
+
+    # Set the GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED_PIN, GPIO.OUT)
 
     # Get a server socket
     s_sock = None
@@ -29,7 +35,10 @@ if __name__ == "__main__":
             log.info("Received: {}".format(data))
             current_state = ~current_state
             log.info("Current State: {}".format(current_state))
+            GPIO.output(LED_PIN, GPIO.LOW if not current_state else GPIO.HIGH)
             c_sock.close()
 
         except Exception as e:
             log.error(traceback.format_exc())
+
+    GPIO.cleanup()
